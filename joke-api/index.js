d4887504751fd37213a724bd9825559f13b36aca
Om-Alve <omalve8174@gmail.com>
2024-03-24 19:19:17 +0530

    Added addjoke route

--- joke-api/index.js
+++ joke-api/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const jokes = require('./jokes.json')
 const app = express();
+const fs = require('fs');
 
+app.use(express.urlencoded({extended:false}));
 
 app.get('/joke',(req,res)=>{
     const index = Math.floor((Math.random() * jokes.length ));
@@ -14,5 +16,18 @@ app.get('/joke/:type',(req,res)=>{
     res.json(relatedjokes[index]); 
 })
 
+app.post('/addjoke',(req,res)=>{
+    const data = req.body;
+    const newJoke = {id:jokes.length + 1,...data};
+    jokes.push(newJoke);
+    fs.writeFile('./jokes.json',JSON.stringify(jokes),()=>{
+        try{
+            res.json({'status':'successful','id':`${jokes.length.toString()}`});
+        }
+        catch(error){
+            res.json({'error':error});
+        }
+    })
+})
 
 app.listen(8000,()=>console.log(`Server has started on port 8000.`))
